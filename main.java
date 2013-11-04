9679979befc8fbfa126b3bcdc2e4d9fc872e8add
Joel <joelpelton@hotmail.com>
2013-11-04 16:47:30 -0600

    Added first class

---
+++ main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+class Hobbit {
+	public static void main( String[] args ) {
+		JFrame 	myWindow;
+		myWindow = new JFrame();
+		myWindow.setSize(300,200);
+		myWindow.setTitle("There and Back Again");
+		myWindow.setVisible(true);
+	}
+}
