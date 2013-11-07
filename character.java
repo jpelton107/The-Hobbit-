60cc139b0df417256d2277ec5feacf7abd9dec80
Joel <joelpelton@hotmail.com>
2013-11-06 22:17:45 -0600

    work on character class

---
+++ character.java
@@ -0,0 +1,20 @@
+public class Character {
+	private boolean alive;
+	private int health;
+	private String name;
+	private int intelligence;
+	private int strength;
+	private int agility;
+	private int weight;
+	
+	Character() {
+		health = 50;
+		alive = true;
+		name = "Bilbo Baggins";
+		intelligence = 5;
+		strength = 5;
+		agility = 5;
+		weight = 100;
+	}
+
+}
--- main.java
+++ main.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
+import java.util.*;
 class Hobbit {
 	public static void main( String[] args ) {
 		JFrame 	myWindow;
 		myWindow = new JFrame();
 		myWindow.setSize(300,200);
 		myWindow.setTitle("There and Back Again.");
+		System.out.println("There and Back Again");
 		myWindow.setVisible(true);
 	}
 }
